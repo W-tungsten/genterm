32563e28fc5a14dd7ea69403d013ed21c09b1c0e
W-tungsten <129035838+W-tungsten@users.noreply.github.com>
2023-06-03 01:26:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,212 @@
-use std::{io, io::Write};
+// This will fairly soon have YAML parsing for proper configuration.
+// Using this terminal prompt (at least for now) pretty much _requires_ you to have the following:
+// A Nerd Font
+// 24-bit color terminal support
+// main_old.rs here is deprecated and also just terrible code
 
-// Types of elements
-#[derive(Clone)]
-enum Element {
-    SharpGtArrow(bool, u8, u8, u8, u8, u8, u8),
-    SoftGtArrow(bool, u8, u8, u8, u8, u8, u8),
-    SharpLtArrow(bool, u8, u8, u8, u8, u8, u8),
-    SoftLtArrow(bool, u8, u8, u8, u8, u8, u8),
-    CommandResult(bool, String, u8, u8, u8, u8, u8, u8),
-    VariableValue(bool, String, u8, u8, u8, u8, u8, u8),
-    Text(bool, String, u8, u8, u8, u8, u8, u8)
+// Do not do any of the following:
+// Distribute this code verbatim or >= 95% verbatim without a link to the GitHub at https://github.com/W-tungsten/genterm
+// (Or a similar link taking you to the GitHub source)
+// Claim that the code verbatim or >= 95% verbatim is yours
+// Distribute this code verbatim or >= 95% verbatim without the comments from the line saying
+// "Do not do any of the following:" onwards.
+// Otherwise, anyone can do anything they like with this code.
+// (This also applies to main_old.rs)
+
+static ARROW_TYPES: &[ElementType] = &[
+    ElementType::SharpGtArrow,
+    ElementType::SharpLtArrow,
+    ElementType::SoftGtArrow,
+    ElementType::SoftLtArrow,
+];
+
+use std::{fs::File, io, io::Read, io::Write};
+
+#[derive(Copy, Clone, PartialEq)]
+enum ElementType {
+    SharpGtArrow,
+    SharpLtArrow,
+    SoftGtArrow,
+    SoftLtArrow,
+    CommandResult,
+    VariableValue,
+    Text,
 }
 
-fn main() -> std::io::Result<()> {
-    let elements: Vec<Element> = vec![
-        // Change this bit
-        // The first boolean is whether to ignore background colour
-        // If it is false, just set the last 3 numbers to 0, they will be ignored
-        // To save your changes, run cargo build -r -v in this directory then restart bash
-        Element::CommandResult(false, String::from("pwd"), 2, 177, 252, 113, 255, 236),
-        Element::Text(true, String::from(" "), 2, 177, 252, 0, 0, 0),
-        Element::Text(false, String::from(" at "), 119, 89, 0, 252, 189, 2),
-        Element::Text(false, String::from(" "), 71, 71, 71, 221, 221, 221),
-        Element::CommandResult(true, String::from("date +%H:%M:%S"), 71, 71, 71, 0, 0, 0),
-        Element::SharpGtArrow(true, 71, 71, 71, 0, 0, 0),
+#[derive(Clone)]
+enum ElementContents {
+    StringColors(String, Option<Color>, Option<Color>),
+    Colors(Option<Color>, Option<Color>),
+}
+
+#[derive(Clone)]
+struct Element {
+    variant: ElementType,
+    contents: ElementContents,
+}
+
+#[derive(Clone, Copy, Default, PartialEq, Debug)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+trait Render {
+    fn render(self) -> String;
+}
+
+trait Correct {
+    fn correct(self) -> Element;
+}
+
+impl Correct for Vec<Element> {
+    fn correct(self) -> Element {
+        /* *** PLEASE NOTE THAT THIS FUNCTION ASSUMES THAT THE FIRST ELEMENT IS AN ARROW.
+        IF IT ISN'T THIS FUNCTION IS UNNECESSARY. *** */
+        Element {
+            variant: self[0].variant,
+            contents: match (self[0].contents.clone(), self[1].contents.clone()) {
+                (ElementContents::Colors(_, fg), ElementContents::Colors(_, bg))
+                | (ElementContents::Colors(_, fg), ElementContents::StringColors(_, bg, _)) => {
+                    ElementContents::Colors(fg, bg)
+                }
+                _ => unimplemented!(),
+            },
+        }
+    }
+}
+
+impl Render for Vec<Element> {
+    fn render(self) -> String {
+        let mut output = String::new();
+
+        for window in self.windows(2) {
+            output += &if ARROW_TYPES.contains(&window[0].variant) {
+                    window.to_vec().correct()
+                } else {
+                    window[0].clone()
+                }
+                .render();
+        }
+
+        if let Some(last) = self.last() {
+            output += &if ARROW_TYPES.contains(&last.variant) {
+                vec![last.clone(), Element {
+                    variant: ElementType::Text,
+                    contents: ElementContents::StringColors("".to_owned(), None, None),
+                }].correct()
+            } else {
+                last.clone()
+            }
+                .render();
+        }
+
+
+
+        if !output.is_empty() {
+            output + r"\x01\e[0m\x02"
+        } else {
+            output
+        }
+    }
+}
+
+impl Render for Element {
+    fn render(self) -> String {
+        let (variant, contents) = (self.variant, self.contents);
+        if [
+            ElementType::SharpGtArrow,
+            ElementType::SharpLtArrow,
+            ElementType::SoftGtArrow,
+            ElementType::SharpLtArrow,
+        ]
+        .contains(&variant)
+        {
+            let (fg, bg) = match contents {
+                ElementContents::Colors(fg, bg) => (fg, bg),
+                _ => unreachable!(),
+            };
+            (fg, bg).sequence()
+                + match variant {
+                    ElementType::SharpGtArrow => "",
+                    ElementType::SharpLtArrow => "",
+                    ElementType::SoftGtArrow => "",
+                    ElementType::SoftLtArrow => "", //      ▓▒░    ░▒▓ 
+                    _ => unreachable!(),
+                }
+        } else {
+            let (str, fg, bg) = match contents {
+                ElementContents::StringColors(str, fg, bg) => (str, fg, bg),
+                _ => unreachable!(),
+            };
+            (fg, bg).sequence()
+                + &match variant {
+                    ElementType::CommandResult => format!(r"\x01$({})\x02", str),
+                    ElementType::Text => str,
+                    ElementType::VariableValue => format!("${}", str),
+                    _ => unreachable!(),
+                }
+        }
+    }
+}
+
+trait Sequence {
+    fn sequence(self) -> String;
+}
+
+impl Sequence for (Option<Color>, Option<Color>) {
+    fn sequence(self) -> String {
+        let mut output = String::new();
+
+        if let Some(fg) = self.0 {
+            output += &format!(r"\x1b[38;2;{};{};{}m", fg.r, fg.g, fg.b);
+        }
+
+        if let Some(bg) = self.1 {
+            output += &format!(r"\x1b[48;2;{};{};{}m", bg.r, bg.g, bg.b);
+        }
+
+        if &output != "" {
+            r"\x01".to_owned() + &output + r"\x02"
+        } else {
+            output
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    /*let default_config = "".as_bytes();
+    let mut file_contents = String::new();
+    if let Ok(mut file) = File::open("~/.genterm/config.toml") {
+        file.read_to_string(&mut file_contents).unwrap_or_else(|_| {
+            eprintln!(
+                "Failed to read config file ...
+Attempting to create config file with default values ..."
+            );
+            0
+        });
+    } else {
+        if let Ok(mut file) = File::create("~/.genterm/config.txt") {
+            file.write_all(default_config)
+                .expect("Failed to make config file");
+        }
+    }*/
+
+    let v = vec![
+        Element {
+            variant: ElementType::CommandResult,
+            contents: ElementContents::StringColors(
+            "pwd".to_owned(),
+             Some(Color { r: 0, g: 255, b: 0 }),
+              Some(Color { r: 255, g: 0, b: 255 }))
+        },
+        Element {
+            variant: ElementType::SharpGtArrow,
+            contents: ElementContents::Colors(None, None),
+        }
     ];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    print!("{}", render(elements)?);
-    io::stdout().flush()?;
+    println!("{}", v.render());
     Ok(())
 }
-
-impl Element {
-    fn ignore_backgnd(self) -> bool {
-        match self {
-            Element::CommandResult(b, ..) => b,
-            Element::SharpGtArrow(b, ..) => b,
-            Element::SharpLtArrow(b, ..) => b,
-            Element::SoftGtArrow(b, ..) => b,
-            Element::SoftLtArrow(b, ..) => b,
-            Element::Text(b, ..) => b,
-            Element::VariableValue(b, ..) => b,
-        }
-    }
-}
-
-fn colour(include_backgnd: bool, element: &str, foreground: (u8, u8, u8), background: (u8, u8, u8),) -> String {
-    // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797 for help with colouring stuff in the terminal
-    if include_backgnd {
-        format!("\\x1b[38;2;{};{};{}m{}", foreground.0, foreground.1, foreground.2, element)
-    } else {
-        format!("\\x1b[38;2;{};{};{}m\\x1b[48;2;{};{};{}m{}", foreground.0, foreground.1, foreground.2, background.0, background.1, background.2, element)
-    }
-}
-
-fn render(elements: Vec<Element>) -> std::io::Result<String> {
-    let mut output = String::new();
-    for element in elements {
-        if element.clone().ignore_backgnd() == true {
-            output = format!("{}{}", output, match element {
-                Element::SharpGtArrow(_, a, b, c, _, _, _) => colour(true, ">", (a, b, c), (0, 0, 0)),
-                Element::SoftGtArrow(_, a, b, c, _, _, _) => colour(true, ")", (a, b, c), (0, 0, 0)),
-                Element::SharpLtArrow(_, a, b, c, _, _, _) => colour(true, "<", (a, b, c), (0, 0, 0)),
-                Element::SoftLtArrow(_, a, b, c, _, _, _) => colour(true, "(", (a, b, c), (0, 0, 0)),
-                Element::CommandResult(_, comm, a, b, c, _, _, _) => colour(true, &format!("$({})", comm), (a, b, c), (0, 0, 0)),
-                Element::VariableValue(_, name, a, b, c, _, _, _) => colour(true, &format!("${}", name), (a, b, c), (0, 0, 0)),
-                Element::Text(_, text, a, b, c, _, _, _) => colour(true, &text, (a, b, c), (0, 0, 0)),
-            });
-            continue;
-        }
-        output = format!("{}{}", output, match element {
-            Element::SharpGtArrow(_, a, b, c, d, e, f) => colour(false, ">", (a, b, c), (d, e, f)),
-            Element::SoftGtArrow(_, a, b, c, d, e, f) => colour(false, ")", (a, b, c), (d, e, f)),
-            Element::SharpLtArrow(_, a, b, c, d, e, f) => colour(false, "<", (a, b, c), (d, e, f)),
-            Element::SoftLtArrow(_, a, b, c, d, e, f) => colour(false, "(", (a, b, c), (d, e, f)),
-            Element::CommandResult(_, comm, a, b, c, d, e, f) => colour(false, &format!("$({})", comm), (a, b, c), (d, e, f)),
-            Element::VariableValue(_, name, a, b, c, d, e, f) => colour(false, &format!("${}", name), (a, b, c), (d, e, f)),
-            Element::Text(_, text, a, b, c, d, e, f) => colour(false, &text, (a, b, c), (d, e, f)),
-        });        
-    }
-    output += "\\x1b[0m";
-    output += " ";
-    Ok(output)
-}
-
-
